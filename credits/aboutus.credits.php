f78c3e93962bb4d887d80118cb61a97cfded635d
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-05-02 23:08:40 +0300

    Added aboutus page

--- credits/aboutus.credits.php
+++ credits/aboutus.credits.php
@@ -1,1 +1,38 @@
-<?php
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tenant Management System : Manager</title>
+    <link rel="icon" href="/images/favicon.png" type="image/png"/>
+    <link rel="shortcut icon" href="/images/favicon.png" type="image/png"/>
+    <script src="//cdn.tms-dist.lan:433/styles/js/ver.js"></script>
+    <script src="//cdn.tms-dist.lan:433/styles/js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+    <script src="//cdn.tms-dist.lan:433/styles/js/navigation.min.js"></script>
+    <script src="//cdn.tms-dist.lan:433/styles/js/main.js" async></script>
+    <script src="//cdn.tms-dist.lan:433/styles/js/jquery-2.1.3.min.js" crossorigin="anonymous"></script>
+    <script src="//cdn.tms-dist.lan:433/styles/js/tenants.min.js"></script>
+    <link rel="stylesheet" href="//cdn.tms-dist.lan:433/styles/css/bootstrap.min.css" type="text/css"/>
+    <link rel="stylesheet" href="//cdn.tms-dist.lan:433/styles/css/man-custom.css" type="text/css"/>
+    <link rel="stylesheet" href="//cdn.tms-dist.lan:433/styles/css/custom.css" type="text/css"/>
+</head>
+
+<body>
+<?php session_start();
+//if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true && $_SESSION['manager_logged_in'] == true) { ?>
+<div id="header"></div>
+<div id="dialog"></div>
+<div class="container">
+    <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="junior-lawrence-kibirige-94709a16b"><a class="LI-simple-link" href='https://ug.linkedin.com/in/junior-lawrence-kibirige-94709a16b?trk=profile-badge'>Junior Lawrence Kibirige</a></div>
+</div>
+<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+<script src="//cdn.tms-dist.lan:433/styles/js/popper.min.js"></script>
+<script src="//cdn.tms-dist.lan:433/styles/js/bootstrap.min.js"></script>
+<script src="//cdn.tms-dist.lan:433/styles/js/jquery-2.1.3.min.js"></script>
+<script>
+    loadprerequisites();
+</script>
+</body>
+
+</html>
